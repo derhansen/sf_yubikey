320fd5e60ece7983a237a74a8c837ef6e2697646
Torben Hansen <derhansen@gmail.com>
2015-05-03 19:36:27 +0200

    [WIP] Updated login template for TYPO3 7.3+ refs #26

--- ext_tables.php
+++ ext_tables.php
@@ -29,14 +29,17 @@ if ($version[0] == 7) {
 	$template = 'typo3conf/ext/sf_yubikey/Resources/Private/Templates/login-v7.html';
 }
 
-if ($version[0] == 7 && $version[1] >= 2) {
-	$tmplPath = 'EXT:backend/Resources/Private/Templates/login.html';
-	$template = 'typo3conf/ext/sf_yubikey/Resources/Private/Templates/Login-v72.html';
-}
-
 if (isset($extConf['yubikeyEnableBE']) && (bool)$extConf['yubikeyEnableBE']) {
+	// For TYPO3 6.2.x to 7.1.x
 	$TBE_STYLES['htmlTemplates'][$tmplPath] = PATH_site . $template;
 	$TBE_STYLES['stylesheet2'] = '../typo3conf/ext/sf_yubikey/Resources/Public/Css/sf_yubikey.css';
+
+	// Since TYPO3 7.3
+	if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginTemplate']['formTemplates'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginTemplate']['formTemplates'] = array();
+	}
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginTemplate']['formTemplates']['sfyubikey'] = 'EXT:sf_yubikey/Resources/Private/Templates/Login-v72.html';
+
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users',$tempColumns,1);
