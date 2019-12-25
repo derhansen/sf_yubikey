ddb8f0b900544b9174533cec043800943fe45ed8
Torben Hansen <derhansen@gmail.com>
2019-12-25 13:37:44 +0100

    [TASK] Updated composer dependiencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '2.3.0',
+    'version' => '3.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.99-9.5.99',
+            'typo3' => '9.5.99-10.4.99',
             'php' => '7.2.0-7.3.99',
         ],
         'conflicts' => [],
