1c774be07b0d526916992f7fa2b3fdb973f7d180
Torben Hansen <derhansen@gmail.com>
2019-12-25 13:32:02 +0100

    [TASK] Migrate stylesheet2 to $GLOBALS['TBE_STYLES']

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
     // Add css file for YubiKey backend login
-    $TBE_STYLES['stylesheet2'] = '../typo3conf/ext/sf_yubikey/Resources/Public/Css/sf_yubikey.css';
+    $GLOBALS['TBE_STYLES']['stylesheet2'] = '../typo3conf/ext/sf_yubikey/Resources/Public/Css/sf_yubikey.css';
 
     // Add YubiKey fields to user settings
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_sfyubikey_yubikey_enable'] = [
