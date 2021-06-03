8239578c8c005db1c0f1d20a7faef34385810575
Torben Hansen <derhansen@gmail.com>
2021-06-03 15:40:36 +0200

    [TASK] Added new YubiKeyService which uses psr/http-client

---
+++ Classes/Service/YubikeyService.php
@@ -0,0 +1,169 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Extension "sf_yubikey" for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace DERHANSEN\SfYubikey\Service;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Client\NetworkExceptionInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Provides YubiKey authentication using the yubico YubiCloud
+ */
+class YubikeyService
+{
+    protected ClientInterface $httpClient;
+    protected RequestFactoryInterface $requestFactory;
+    protected array $errors = [];
+    protected string $yubikeyClientId = '';
+    protected string $yubikeyClientKey = '';
+    protected array $yubikeyApiUrl = [];
+    protected bool $initialized = false;
+
+    public function __construct(
+        ClientInterface $httpClient,
+        RequestFactoryInterface $requestFactory
+    ) {
+        $this->httpClient = $httpClient;
+        $this->requestFactory = $requestFactory;
+        $extConfig = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('sf_yubikey');
+
+        $this->yubikeyClientId = trim($extConfig['yubikeyClientId']);
+        $this->yubikeyClientKey = trim($extConfig['yubikeyClientKey']);
+        $this->yubikeyApiUrl = GeneralUtility::trimExplode(';', $extConfig['yubikeyApiUrls'], true);
+
+        $this->initialized = $this->yubikeyClientId !== '' && $this->yubikeyClientKey !== '' &&
+            !empty($this->yubikeyApiUrl);
+    }
+
+    /**
+     * Verify HMAC-SHA1 signature on result received from Yubico server
+     *
+     * @param string $response
+     * @param string $yubicoClientKey
+     * @return bool
+     */
+    public function verifyHmac(string $response, string $yubicoClientKey): bool
+    {
+        $lines = GeneralUtility::trimExplode(chr(10), $response);
+        $result = [];
+
+        // Create array from data
+        foreach ($lines as $line) {
+            $lineparts = GeneralUtility::trimExplode('=', $line, false, 2);
+            if ($lineparts[0] !== '') {
+                $result[$lineparts[0]] = trim($lineparts[1]);
+            }
+        }
+
+        // Sort array Alphabetically based on keys
+        ksort($result);
+
+        // Grab the signature sent by server, and delete
+        $signature = $result['h'];
+        unset($result['h']);
+
+        // Build new string to calculate hmac signature on
+        $datastring = '';
+        foreach ($result as $key => $value) {
+            $datastring !== '' ? $datastring .= '&' : $datastring .= '';
+            $datastring .= $key . '=' . $value;
+        }
+        $hmac = base64_encode(hash_hmac('sha1', utf8_encode($datastring), base64_decode($yubicoClientKey), true));
+
+        $valid = $hmac === $signature;
+        if (!$valid) {
+            $this->addError('Could not verify signature');
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Call the Auth API at Yubico server
+     *
+     * @param string $otp
+     * @return bool
+     */
+    public function verifyOtp(string $otp): bool
+    {
+        $requestParams['id'] = $this->yubikeyClientId;
+        $requestParams['otp'] = trim($otp);
+        $requestParams['nonce'] = md5(uniqid((string)rand()));
+        ksort($requestParams);
+        $parameters = '';
+        foreach ($requestParams as $p => $v) {
+            $parameters .= '&' . $p . '=' . $v;
+        }
+        $parameters = ltrim($parameters, '&');
+        $signature = base64_encode(
+            hash_hmac(
+                'sha1',
+                $parameters,
+                base64_decode($this->yubikeyClientKey),
+                true
+            )
+        );
+        $signature = preg_replace('/\+/', '%2B', $signature);
+        $parameters .= '&h=' . $signature;
+        $urls = [];
+        foreach ($this->yubikeyApiUrl as $apiUrl) {
+            $urls[] = $apiUrl . '?' . $parameters;
+        }
+
+        foreach ($urls as $url) {
+            $request = $this->requestFactory->createRequest('GET', $url);
+            try {
+                $response = $this->httpClient->sendRequest($request);
+                if ($response->getStatusCode() !== 200) {
+                    $this->addError('HTTP_STATUS_CODE_' . $response->getStatusCode());
+                    continue;
+                }
+                $data = (string)$response->getBody();
+                if ($this->verifyHmac($data, $this->yubikeyClientKey)) {
+                    if (!preg_match('/status=([a-zA-Z0-9_]+)/', $data, $result)) {
+                        return false;
+                    }
+                    if ($result[1] === 'OK') {
+                        return true;
+                    }
+                    $this->addError($result[1]);
+                }
+            } catch (NetworkExceptionInterface $e) {
+                $this->addError('NETWORK_EXCEPTION');
+                continue;
+            } catch (ClientExceptionInterface $e) {
+                $this->addError('CLIENT_EXCEPTION');
+                continue;
+            }
+        }
+
+        return false;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function isInitialized(): bool
+    {
+        return $this->initialized;
+    }
+
+    protected function addError(string $error): void
+    {
+        $this->errors[] = $error;
+    }
+}
---
+++ Tests/Unit/YubikeyServiceTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace DERHANSEN\SfYubikey\Tests\Unit;
+
+/*
+ * This file is part of the Extension "sf_yubikey" for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use DERHANSEN\SfYubikey\Service\YubikeyService;
+use TYPO3\CMS\Core\Http\Client\GuzzleClientFactory;
+use TYPO3\CMS\Core\Http\RequestFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * Testcase for class DERHANSEN\SfYubikey\Service\YubikeyAuthService
+ */
+class YubikeyServiceTest extends UnitTestCase
+{
+    public function verifyHmacDataProvider()
+    {
+        $h = 'lHVE5KrfuhWg36MttJOwxWqa/AY=';
+        $t = '2021-02-13T05:20:11Z0768';
+        $otp = 'abcdefghabcdefghabcdefghabcdefghabcdefghabcd';
+        $nonce = '098f6bcd4621d373cade4e832627b4f6';
+        $status = 'OK';
+        $clientKey = 'SomEraNd0MCliEnTK3y=';
+
+        return [
+            'valid response' => [
+                'h=' . $h . "\n" . 't=' . $t . "\n" . 'otp=' . $otp . "\n" . 'nonce=' . $nonce . "\n" . 'status=' . $status,
+                $clientKey,
+                true
+            ],
+            'wrong api key' => [
+                'h=' . $h . "\n" . 't=' . $t . "\n" . 'otp=' . $otp . "\n" . 'nonce=' . $nonce . "\n" . 'status=' . $status,
+                'invalid',
+                false
+            ],
+            'status modified' => [
+                'h=' . $h . "\n" . 't=' . $t . "\n" . 'otp=' . $otp . "\n" . 'nonce=' . $nonce . "\n" . 'status=' . 'NOK',
+                $clientKey,
+                false
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider verifyHmacDataProvider
+     */
+    public function verifyHmacWorksAsExpected($response, $apiKey, $expected)
+    {
+        // Dummy extension settings
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['sf_yubikey'] = [
+            'yubikeyClientId' => 'test',
+            'yubikeyClientKey' => 'test',
+            'yubikeyApiUrls' => 'api1,api2',
+        ];
+
+        $yubikeyAuthService = GeneralUtility::makeInstance(
+            YubikeyService::class,
+            GuzzleClientFactory::getClient(),
+            new RequestFactory
+        );
+        self::assertSame($expected, $yubikeyAuthService->verifyHmac($response, $apiKey));
+    }
+}
