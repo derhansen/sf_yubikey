32da94e25c35e3c4b25585db7f8fce6a5ffb5fa1
Torben Hansen <derhansen@gmail.com>
2021-10-06 08:07:24 +0200

    [RELEASE] Release of version 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'uploadfolder' => 0,
     'clearCacheOnLoad' => 1,
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.4.0-11.5.99'
+            'typo3' => '11.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
