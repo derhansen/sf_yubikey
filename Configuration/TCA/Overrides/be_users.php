a3e3674b978e748be761d8f22f0ceb9879845cd2
Torben Hansen <derhansen@gmail.com>
2026-04-21 09:05:33 +0200

    [TASK] Migrate user settings to TCA
    Refs #71
    
    Signed-off-by: Torben Hansen <derhansen@gmail.com>

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -38,3 +38,20 @@ ExtensionManagementUtility::addToAllTCAtypes(
     'be_users',
     '--div--;YubiKey,tx_sfyubikey_yubikey_enable, tx_sfyubikey_yubikey_id'
 );
+
+// Show new fields in backend user settings
+$GLOBALS['TCA']['be_users']['columns']['user_settings']['columns']['tx_sfyubikey_yubikey_enable'] = [
+    'label' => 'LLL:EXT:sf_yubikey/Resources/Private/Language/locallang_db.xlf:users.tx_sfyubikey_yubikey_enable',
+    'config' => [
+        'type' => 'check',
+        'renderType' => 'checkboxToggle',
+    ],
+];
+$GLOBALS['TCA']['be_users']['columns']['user_settings']['columns']['tx_sfyubikey_yubikey_id'] = [
+    'label' => 'LLL:EXT:sf_yubikey/Resources/Private/Language/locallang_db.xlf:users.tx_sfyubikey_yubikey_id',
+    'config' => [
+        'type' => 'text',
+    ],
+];
+
+$GLOBALS['TCA']['be_users']['columns']['user_settings']['showitem'] .= ',--div--;YubiKey,tx_sfyubikey_yubikey_enable, tx_sfyubikey_yubikey_id';
